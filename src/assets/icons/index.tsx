da3c4f04af86916948121dfb90d57a4aa89ce92f
Klinger Matheus <kger.matheus@gmail.com>
2025-03-07 02:15:41 -0300

    chore: adicionado novos icons como: instagram, linkedin e github.

---
+++ src/assets/icons/index.tsx
@@ -0,0 +1,5 @@
+import LinkedInIcon from "./linkedin-icon.svg";
+import GithubIcon from "./github-icon.svg";
+import InstagramIcon from "./instagram-icon.svg";
+
+export { LinkedInIcon, GithubIcon, InstagramIcon };
