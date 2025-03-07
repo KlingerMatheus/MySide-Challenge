310dc2b025772f4c929f91cacbf48430a5121ca6
Klinger Matheus <kger.matheus@gmail.com>
2025-03-07 02:22:52 -0300

    feat: Implementação do Footer como componente.

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,3 +1,52 @@
+import Link from "next/link";
+
+import { GithubIcon, InstagramIcon, LinkedInIcon } from "@/assets/icons";
+import styles from "./footer.module.css";
+import Image from "next/image";
+
 export const Footer = () => {
-  return <footer>Footer</footer>;
+  const socialMedias = [
+    {
+      id: "linkedin",
+      icon: LinkedInIcon,
+      path: "https://linkedin.com/in/klinger-matheus",
+    },
+    {
+      id: "github",
+      icon: GithubIcon,
+      path: "https://github.com/klingermatheus",
+    },
+    {
+      id: "instagram",
+      icon: InstagramIcon,
+      path: "https://instagram.com/klinger_matheus",
+    },
+  ] as const;
+
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.container}>
+        <div className={styles.message}>
+          This website is developed by <b>Klinger Matheus</b>, in purpose to
+          complete the <abbr title="Front End">FE</abbr> challenge provided by{" "}
+          <strong>
+            <Link href="https://myside.com.br/">MySide</Link>
+          </strong>
+          .
+        </div>
+        <div className={styles["social-media-container"]}>
+          {socialMedias.map((socialMedia) => (
+            <Link key={socialMedia.id} target="_blank" href={socialMedia.path}>
+              <Image
+                src={socialMedia.icon}
+                alt={socialMedia.id}
+                height={30}
+                width={30}
+              />
+            </Link>
+          ))}
+        </div>
+      </div>
+    </footer>
+  );
 };
