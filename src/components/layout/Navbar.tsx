acb99613d1595e7633991f9d85f3fa217299f3f0
Klinger Matheus <kger.matheus@gmail.com>
2025-03-06 23:14:47 -0300

    feat: Implementação do componente `<Navbar />`.

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,3 +1,38 @@
+import Image from "next/image";
+import Link from "next/link";
+import styles from "./navbar.module.css";
+import { ShoppingCartIcon } from "@heroicons/react/24/solid";
+
 export const Navbar = () => {
-  return <nav>Navbar</nav>;
+  const cartItemsQuantity = 2; // While Store was not implemented yet, this value is being mocked
+
+  return (
+    <nav className={styles.nav}>
+      <div className={styles["logo-wrapper"]}>
+        <Image
+          className={styles.logo}
+          src="/my-side-logo.jpg"
+          alt="MySide logo"
+          width={50}
+          height={50}
+          loading="lazy"
+        />
+        <span>MySide</span>
+      </div>
+      <ul className={styles["nav-links"]}>
+        <li className={styles["nav-link"]}>
+          <Link href="/">View products</Link>
+        </li>
+        <span className={styles.divider} />
+        <li className={styles["cart-button"]}>
+          <Link href="/cart">
+            <span className={styles["cart-items-count"]}>
+              {cartItemsQuantity <= 9 ? cartItemsQuantity : "9+"}
+            </span>
+            <ShoppingCartIcon height={32} />
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
 };
